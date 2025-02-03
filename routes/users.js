77fd67aec067daa3dbc872551914da52f542a063
FranciscoMaiaru <franciscomaiaru98@gmail.com>
2025-02-03 20:59:32 -0300

    Se eliminaron carpetas innecesarias

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controllers = require('../controller/controller');
-
-
-/* GET users listing. */
-router.get('/', controllers.myUser);
-router.post('/createUser', controllers.newUser);
-module.exports = router;
