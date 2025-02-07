0fd1962ccfaf893308768f708c2e2ce9d9d697c6
FranciscoMaiaru <franciscomaiaru98@gmail.com>
2025-02-07 09:20:15 -0300

    Modifico el index para que funcione mientras se trabaja en el uso de la API externa.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const controllers = require('../controller/controller');
 
+// La idea es usar una API externa para cargar la página de inicio. Es solo un ejemplo, luego no hay que interactuar con el frontend para este proyecto.
 /* GET home page. */
-router.get('/', controllers.myIndex);
+router.get('/', async (_,res) => {
+    try {
+        fetch('url')
+            .then()
+            .then().render();
+        res.status(201).json({msg: 'Pagina index cargada a traves de una API externa'});
+    } catch (error) {
+        res.status(501).json({msg: 'no se puedo conectar a la API externa', error});
+    }
+} );
 
 module.exports = router;
